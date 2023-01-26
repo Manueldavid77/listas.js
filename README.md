dca14151c5e656b9509fa9cc52415fc260651c85
Manueldavid77 <81543336+Manueldavid77@users.noreply.github.com>
2023-01-26 15:14:25 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# listas.js
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-k9m53t)
---
+++ index.js
@@ -0,0 +1,14 @@
+let nombre = "Manuel"
+let edad = 26
+let desarrollador = (true)
+
+const libro = {
+  titulo:" El Developer",
+  autor: "Armando Wattson",
+  fecha: 2015,
+  url: "aqui va la urla",
+
+}
+
+
+
